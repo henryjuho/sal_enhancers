32147687eb3d4fc66e2bf8e185e5d93dba2725bd
henryjuho <juhob@hotmail.co.uk>
2020-02-12 10:54:52 +0200

    mutiz jobs

--- homeoblock_alignments/chromo_pair_multi.py
+++ homeoblock_alignments/chromo_pair_multi.py
@@ -1,13 +1,15 @@
 import argparse
 import os
+import shutil
+from qsub import q_sub
 
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-chromo_pairs')
-    parser.add_argument('-key')
-    parser.add_argument('-in_dir')
+    parser.add_argument('-chromo_pairs', help='CSV of chromo comparissions', required=True)
+    parser.add_argument('-key', help='CSV of chromosome names and corresponding refseq IDs', required=True)
+    parser.add_argument('-in_dir', help='top level input directory', required=True)
     args = parser.parse_args()
 
     keys = {x.split(',')[1].rstrip(): x.split(',')[0] for x in open(args.key)}
@@ -21,10 +23,10 @@ def main():
         chr_x = keys[chr_x]
         chr_y = keys[chr_y]
 
-        maf_1_dir = args.in_dir + '/' + chr_x + '/'
+        maf_1_dir = args.in_dir + chr_x + '/'
         maf_1 = [x for x in os.listdir(maf_1_dir) if x.endswith('1.sing.maf')][0]
 
-        maf_2_dir = args.in_dir + '/' + chr_y + '/'
+        maf_2_dir = args.in_dir + chr_y + '/'
         maf_2 = [x for x in os.listdir(maf_2_dir) if x.endswith('b.sing.maf')][0]
 
         out_dir = args.in_dir + chr_x + '_vs_' + chr_y + '/'
@@ -35,13 +37,27 @@ def main():
         out_maf = chr_x + '_vs_' + chr_y + '.maf'
 
         print('creating: ' + out_dir)
-        print('copying: ' + maf_1_dir + maf_1 + ' -> ' + out_dir + a_maf)
-        print('copying: ' + maf_2_dir + maf_2 + ' -> ' + out_dir + b_maf)
-        print('creating: ' + align_dir)
-        print('creating: ' + tmp_dir)
+        os.makedirs(out_dir)
 
-        print('out maf: ' + out_maf)
+        print('copying: ' + maf_1_dir + maf_1 + ' -> ' + out_dir + a_maf)
+        shutil.copy(maf_1_dir + maf_1, out_dir + a_maf)
+
+        print('copying: ' + maf_2_dir + maf_2 + ' -> ' + out_dir + b_maf)
+        shutil.copy(maf_2_dir + maf_2, out_dir + b_maf)
+
+        print('creating: ' + align_dir)
+        os.makedirs(align_dir)
+
+        print('creating: ' + tmp_dir)
+        os.makedirs(tmp_dir)
+
+        print('out maf: ' + out_dir + out_maf)
         print()
+
+        roast = ('~/sal_enhancers/homeoblock_alignments/roast_homeoblock.py '
+                 '-maf_dir {} -ref pike').format(out_dir)
+
+        q_sub([roast], out=out_dir + chr_x + '_vs_' + chr_y, scheduler='SLURM')
 
 
 if __name__ == '__main__':
