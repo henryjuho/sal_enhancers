b77762c4f7c4edcfef8d1ede28ab29de79284ee9
henryjuho <juhob@hotmail.co.uk>
2020-02-13 17:24:31 +0200

    recode for sal ref

--- homeoblock_alignments/chromo_pair_multi.py
+++ homeoblock_alignments/chromo_pair_multi.py
@@ -1,47 +1,16 @@
-import argparse
 import os
-import shutil
 from qsub import q_sub
+import sys
 
 
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-chromo_pairs', help='CSV of chromo comparissions', required=True)
-    parser.add_argument('-key', help='CSV of chromosome names and corresponding refseq IDs', required=True)
-    parser.add_argument('-in_dir', help='top level input directory', required=True)
-    args = parser.parse_args()
+    for line in sys.stdin:
 
-    keys = {x.split(',')[1].rstrip(): x.split(',')[0] for x in open(args.key)}
-
-    a_maf = 'pike.salmon.sing.maf'
-    b_maf = 'pike.salmon_b.sing.maf'
-
-    for line in open(args.chromo_pairs).readlines()[1:]:
-
-        chr_x, chr_y = line.rstrip().split(',')
-        chr_x = keys[chr_x]
-        chr_y = keys[chr_y]
-
-        maf_1_dir = args.in_dir + chr_x + '/'
-        maf_1 = [x for x in os.listdir(maf_1_dir) if x.endswith('1.sing.maf')][0]
-
-        maf_2_dir = args.in_dir + chr_y + '/'
-        maf_2 = [x for x in os.listdir(maf_2_dir) if x.endswith('b.sing.maf')][0]
-
-        out_dir = args.in_dir + chr_x + '_vs_' + chr_y + '/'
+        out_dir = line.rstrip() + '/'
 
         align_dir = out_dir + 'aligned/'
         tmp_dir = align_dir + 'tmp/'
-
-        print('creating: ' + out_dir)
-        os.makedirs(out_dir)
-
-        print('copying: ' + maf_1_dir + maf_1 + ' -> ' + out_dir + a_maf)
-        shutil.copy(maf_1_dir + maf_1, out_dir + a_maf)
-
-        print('copying: ' + maf_2_dir + maf_2 + ' -> ' + out_dir + b_maf)
-        shutil.copy(maf_2_dir + maf_2, out_dir + b_maf)
 
         print('creating: ' + align_dir)
         os.makedirs(align_dir)
@@ -49,12 +18,12 @@ def main():
         print('creating: ' + tmp_dir)
         os.makedirs(tmp_dir)
 
-        print()
-
         roast = ('~/sal_enhancers/homeoblock_alignments/roast_homeoblock.py '
-                 '-maf_dir {} -ref pike').format(out_dir)
+                 '-maf_dir {} -ref salmon').format(out_dir)
 
-        q_sub([roast], out=out_dir + chr_x + '_vs_' + chr_y, scheduler='SLURM')
+        q_sub([roast], out=out_dir + 'multiple_align', scheduler='SLURM')
+
+        print()
 
 
 if __name__ == '__main__':
