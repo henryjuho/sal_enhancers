cafa5f90a64d090015dbbfe5eadb31ab5dd5d463
henryjuho <juhob@hotmail.co.uk>
2020-02-20 13:17:25 +0200

    fasta output added

--- homeoblock_alignments/clean_fastas.py
+++ homeoblock_alignments/clean_fastas.py
@@ -44,8 +44,6 @@ def main():
         raw_len = len(sal) - sal.upper().count('N') - sal.count('-')
         sal_b_aligned = len(sal_b) - sal_b.upper().count('N') - sal_b.count('-')
         pike_aligned = len(pike) - pike.upper().count('N') - pike.count('-')
-        
-        print(raw_len)
 
         # get all '-' and 'N' positions
         trim_positions = set()
@@ -53,16 +51,26 @@ def main():
             for base in 'N-':
                 found_pos = find_all(spp_seq.upper(), base)
                 trim_positions |= set(found_pos)
-        
-        print(len(trim_positions))
 
-        # clean alignment
+        # clean alignment and output
         trim_positions = sorted(list(trim_positions))
-        print(len(trim_positions))
         sal = seq_replace(sal, trim_positions).replace('X', '')
-        print(len(sal))
         sal_b = seq_replace(sal_b, trim_positions).replace('X', '')
         pike = seq_replace(pike, trim_positions).replace('X', '')
+
+        species = ('salmon', 'salmon_b', 'pike')
+        aligns = (sal, sal_b, pike)
+
+        # out fasta
+        with open(args.fa_dir + fasta.replace('.fa', '.clean.fa'), 'w') as fa_out:
+            for spp_i in range(0, len(species)):
+
+                print('>' + species[spp_i], file=fa_out)
+
+                for i in range(0, len(sal), 60):
+                    print(aligns[spp_i][i: i + 60], file=fa_out)
+
+                print(file=fa_out)
 
         # output summary
         print(block, raw_len, sal_b_aligned, pike_aligned, len(sal), round((len(sal)/raw_len) * 100, 3), sep=',')
