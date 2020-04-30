6a888649ff5f4806a62f823a573340b3f134c553
henryjuho <juhob@hotmail.co.uk>
2020-04-30 11:57:54 +0300

    will run enhancer script if subregion file "NA"

--- dfe/chromosomal_bootstrap_sfs_data.py
+++ dfe/chromosomal_bootstrap_sfs_data.py
@@ -21,22 +21,40 @@ def main():
         output_file = '{}sfs_ncall_regional.{}.{}.txt'.format(args.out_dir, args.region, chromo)
         log_file = '{}sfs_ncall_regional.{}.{}.log.txt'.format(args.out_dir, args.region, chromo)
 
-        cmd = ('python ~/sal_enhancers/dfe/bootstrappable_sfs_data.py '
-               '-bed_regs {bed_regs} '
-               '-bed_target {bed_target} '
-               '-vcf {vcf} '
-               '-call_fa {call_fa} '
-               '-region {region} '
-               '-chr {chromo} '
-               '1> {out} '
-               '2> {log}').format(bed_regs=args.bed_regs,
-                                  bed_target=args.bed_targets,
-                                  vcf=args.vcf,
-                                  call_fa=args.call_fa,
-                                  region=args.region,
-                                  chromo=chromo,
-                                  out=output_file,
-                                  log=log_file)
+        if args.bed_regs == 'NA':
+            cmd = ('python ~/sal_enhancers/dfe/bootstrappable_sfs_data_enhancers.py '
+                   '-bed_target {bed_target} '
+                   '-vcf {vcf} '
+                   '-call_fa {call_fa} '
+                   '-region {region} '
+                   '-chr {chromo} '
+                   '1> {out} '
+                   '2> {log}').format(bed_regs=args.bed_regs,
+                                      bed_target=args.bed_targets,
+                                      vcf=args.vcf,
+                                      call_fa=args.call_fa,
+                                      region=args.region,
+                                      chromo=chromo,
+                                      out=output_file,
+                                      log=log_file)
+
+        else:
+            cmd = ('python ~/sal_enhancers/dfe/bootstrappable_sfs_data.py '
+                   '-bed_regs {bed_regs} '
+                   '-bed_target {bed_target} '
+                   '-vcf {vcf} '
+                   '-call_fa {call_fa} '
+                   '-region {region} '
+                   '-chr {chromo} '
+                   '1> {out} '
+                   '2> {log}').format(bed_regs=args.bed_regs,
+                                      bed_target=args.bed_targets,
+                                      vcf=args.vcf,
+                                      call_fa=args.call_fa,
+                                      region=args.region,
+                                      chromo=chromo,
+                                      out=output_file,
+                                      log=log_file)
 
         q_sub([cmd], out=output_file.replace('.txt', ''), mem=8, rmem=8, t=48, scheduler='SLURM')
 
