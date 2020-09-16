f0f95c1dc448f07d94eb79962f08ac7b86e8a411
henryjuho <juhob@hotmail.co.uk>
2020-09-16 16:58:13 +0300

    theme settings

--- sfs/sfs_plots.R
+++ sfs/sfs_plots.R
@@ -10,9 +10,16 @@ raw <- subset(sfs_dat, bs_rep==0)
 sfs <- ggplot(raw, aes(x=freq, y=proportion, fill=region)) +
   geom_bar(stat='identity', position='dodge') +
   scale_fill_viridis(discrete = T) +
-  xlab('Derived allele frequency')
+  xlab('derived allele count') +
+  theme_classic() +
+  theme(legend.title=element_blank(),
+        legend.position = "bottom",
+        legend.text = element_text(colour = "black",size=8),
+        axis.text.y = element_text(colour = "black",size=8),
+        axis.text.x = element_text(colour = "black",size=8),
+        strip.background = element_blank())
 
-png('sfs_plot.png', width=8, height=3, res=320, units='in')
+png('sfs_plot.png', width=8, height=4, res=320, units='in')
 
 sfs
 
