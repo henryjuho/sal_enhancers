3065e7a601c4ef6787cd7cdd1ed8dd070393ec85
henryjuho <juhob@hotmail.co.uk>
2020-10-05 16:24:33 +0300

    minmapq -> 15

--- sfs/chromosomal_angsd.py
+++ sfs/chromosomal_angsd.py
@@ -34,7 +34,7 @@ def main():
         angsd_cmd = ('angsd -bam {} '
                      '-doSaf 1 -fold 1 '
                      '-anc {} '
-                     '-GL 2 -minMapQ 2 -minQ 20 -minInd 31 -setMinDepth 124 -setMaxDepth 496 '
+                     '-GL 2 -minMapQ 15 -minQ 20 -minInd 31 -setMinDepth 124 -setMaxDepth 496 '
                      '-rf {} -doCounts 1 '
                      '-out {}').format(args.bam_list, args.ref, reg_file_name, chromo_out)
 
