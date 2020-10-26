5a50a11000bfe7bbc7c437ebd7ade2c8b90bfd59
henryjuho <juhob@hotmail.co.uk>
2020-10-26 16:57:50 +0200

    spp added

--- divergence/get_region_fasta.py
+++ divergence/get_region_fasta.py
@@ -8,6 +8,7 @@ def main():
     parser.add_argument('-wga', help='whole genome alignment bed', required=True)
     parser.add_argument('-bed', help='bed file of region', required=True)
     parser.add_argument('-region', help='region label', required=True)
+    parser.add_argument('-spp', help='Comma separated species list for output', required=True)
     parser.add_argument('-out_stem', help='output directory and file stem', required=True)
     args = parser.parse_args()
 
@@ -19,8 +20,8 @@ def main():
     fasta_cmd = 'zcat {out_stem}.wga.bed.gz | ~/sal_enhancers/divergence/wga2fa.py -out_stem {out_stem}.wga'.format(
         out_stem=out_stem)
 
-    ape_cmd = 'Rscript ~/sal_enhancers/divergence/k80_div_est.R {out_stem}.wga.fa {region}'.format(
-        out_stem=out_stem, region=args.region)
+    ape_cmd = 'Rscript ~/sal_enhancers/divergence/k80_div_est.R {out_stem}.wga.fa {region} -spp {spp}'.format(
+        out_stem=out_stem, region=args.region, spp=args.spp)
 
     q_sub([bed_cmd, fasta_cmd, ape_cmd], out=args.out_stem, rmem=12, mem=12, scheduler='SLURM')
 
