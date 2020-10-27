b6f0f58746c20d5e4cf7fcf4305d0a59ca94bb3d
henryjuho <juhob@hotmail.co.uk>
2020-10-27 11:51:38 +0200

    updated for salmon

--- divergence/k80_div_est.R
+++ divergence/k80_div_est.R
@@ -1,22 +1,22 @@
 library(ape)
 
-args = commandArgs(TRUE)
+args <- commandArgs(TRUE)
 
-fasta = args[1]
-region = args[2]
+fasta <- args[1]
+region <- args[2]
 
-data = read.FASTA(fasta)
+data <- read.FASTA(fasta)
 
-divergance = dist.dna(data, model = "K80")
+divergance <- dist.dna(data, model = "K80")
 
 cat(paste(attr(divergance, "Labels"), sep='_'))
 cat('\n')
 
-gt_fc = divergance[1]
-zf_fc = divergance[2]
-gt_zf = divergance[3]
+sal_charr <- divergance[1]
+sal_trout <- divergance[2]
+charr_trout <- divergance[3]
 
-gt = (gt_fc - zf_fc + gt_zf) / 2
+sal <- (sal_charr - charr_trout + sal_trout) / 2
 
 cat(region, gt, sep='\t')
 cat('\n')
